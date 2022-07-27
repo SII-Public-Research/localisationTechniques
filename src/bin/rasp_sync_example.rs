de8089deeee0257ba8ff418dc342044f3ddbcf07
thomas.berton <thomas.berton@sii.fr>
2022-07-27 11:33:23 +0200

    test sync sur rasp

---
+++ src/bin/rasp_sync_example.rs
@@ -0,0 +1,74 @@
+use futures::executor::block_on;
+
+use rppal::gpio::{Gpio, OutputPin};
+use rppal::hal::Timer;
+use rppal::spi::{Bus, Mode, SlaveSelect, Spi};
+
+use std::{
+    io::{self, Write},
+    thread,
+    time::Duration,
+};
+
+use localisationtechniques::{ok_or_panic, rtt_ds_algorithms::*, tools::*, uwb_basics::*};
+use localisationtechniques::sync::RpiSync;
+use dw3000::{hl,ll,Ready};
+
+
+fn main()
+{
+    let (mut uwbsensor, mut sync)=init();
+
+    loop {
+
+        uwbsensor = ok_or_panic(uwbsensor.uwb_send(&[0], None), "fail");
+        println!("TIMESTAMP AVANT SYNC : {}", ok_or_panic(uwbsensor.dw3000.ll().tx_time().read(), "fail").tx_stamp());
+        sync.sync();
+
+        uwbsensor = ok_or_panic(uwbsensor.uwb_send(&[0], None), "fail");
+        println!("TIMESTAMP APRES SYNC : {}", ok_or_panic(uwbsensor.dw3000.ll().tx_time().read(), "fail").tx_stamp());
+
+        thread::sleep(Duration::from_millis(500));
+
+
+    }
+    
+
+}
+
+
+fn init() -> (UWBSensor<Spi, OutputPin, Ready>,RpiSync)
+
+{
+    /******************************************************* */
+    /************        BASIC CONFIGURATION      ********** */
+    /******************************************************* */
+    let spi = Spi::new(Bus::Spi1, SlaveSelect::Ss0, 4_500_000, Mode::Mode0).unwrap();
+    let gpio = Gpio::new().unwrap();
+    let cs = gpio.get(16).unwrap().into_output();
+
+    /****************************************************** */
+    /*****                DW3000 RESET              ******* */
+    /****************************************************** */
+
+    let mut reset = gpio
+        .get(4)
+        .expect("Failed to set up RESET PIN")
+        .into_output();
+    reset.set_low();
+    reset.set_high();
+
+    thread::sleep(Duration::from_millis(500));
+
+    // create an UWBSensor
+    let mut uwbsensor = ok_or_panic(
+        UWBSensor::new(spi, cs),
+        "Failed to create an UWBSensor object",
+    );
+
+     // SYNC
+     let gpio_sync = gpio.get(24).unwrap().into_output();
+     let sync = RpiSync::new(gpio_sync);
+
+    (uwbsensor,sync)
+}
